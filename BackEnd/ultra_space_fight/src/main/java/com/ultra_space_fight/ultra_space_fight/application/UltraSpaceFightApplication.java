b5d578b0048a0352b555f4266c00f00677ff6154
Felipe Bianchi Stefens <lipebianchistefens@gmail.com>
2025-08-15 15:41:10 -0300

    Organization

---
+++ BackEnd/ultra_space_fight/src/main/java/com/ultra_space_fight/ultra_space_fight/application/UltraSpaceFightApplication.java
@@ -0,0 +1,15 @@
+package com.ultra_space_fight.ultra_space_fight.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraSpaceFightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraSpaceFightApplication.class, args);
+
+		// Testing output example;
+		System.out.println("Ultra Space Fight application started successfully!");
+	}
+}
---
+++ BackEnd/ultra_space_fight/src/test/java/com/ultra_space_fight/ultra_space_fight/UltraSpaceFightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultra_space_fight.ultra_space_fight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraSpaceFightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
