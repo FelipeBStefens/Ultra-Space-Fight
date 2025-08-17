4bb4dcce728e531ce40aa76528d082b8841a9985
Felipe Bianchi Stefens <lipebianchistefens@gmail.com>
2025-08-16 21:02:03 -0300

    Changing name of Main

--- BackEnd/ultra_space_fight/src/main/java/com/ultra_space_fight/ultra_space_fight/application/MainApplication.java
+++ BackEnd/ultra_space_fight/src/main/java/com/ultra_space_fight/ultra_space_fight/application/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UltraSpaceFightApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UltraSpaceFightApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 
 		// Testing output example;
 		System.out.println("Ultra Space Fight application started successfully!");
